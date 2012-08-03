82ba7d450ed0b05a0d81abc69bdcaa04953a9068
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2012-08-03 12:44:45 +0900

    Added license information.

--- wavelet_matrix.py
+++ wavelet_matrix.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
-
+#
+# Copyright (c) 2012 Hiroshi Manabe (manabe.hiroshi@gmail.com)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above Copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above Copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the authors nor the names of its contributors
+#    may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+#
 from bit_vector_mock import BitVectorMock
 
 class WaveletMatrix(object):
